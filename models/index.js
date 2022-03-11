8e6ca2991a38b3dee7857d0393b6d32fd27daf86
Mark Shaw <93953616+AboutShaw@users.noreply.github.com>
2022-03-11 11:45:30 +0000

    comment order update

--- models/index.js
+++ models/index.js
@@ -2,17 +2,20 @@ const db = require("../db");
 const { articleIds, usernames, testTopics } = require("../db/helpers/utils");
 
 exports.selectTopics = () => {
-    return db
-    .query(`    SELECT  *
-                FROM    topics;`)
+  return db
+    .query(
+      `    SELECT  *
+                FROM    topics;`
+    )
     .then((result) => {
-        return result.rows;
+      return result.rows;
     });
 };
 
 exports.selectArticles = () => {
-    return db
-    .query(`    SELECT  A.author,
+  return db
+    .query(
+      `    SELECT  A.author,
                         A.title,
                         A.article_id,
                         A.topic,
@@ -22,31 +25,35 @@ exports.selectArticles = () => {
                     FROM    articles A
                     LEFT JOIN comments B ON A.article_id=A.article_id
                     GROUP BY A.article_id
-                    ORDER BY created_at DESC;`)
+                    ORDER BY created_at DESC;`
+    )
     .then((result) => {
-        return result.rows;
+      return result.rows;
     });
 };
 
-exports.sortArticles = (sorter=`created_at`, order=`DESC`, topic) => {
-    const sortParams = ['author',
-                        'title',
-                        'article_id',
-                        'topic',
-                        'created_at',
-                        'votes',
-                        'comment_count'];
-    const orderParams = ['ASC', 'DESC'];
-    order = order.toLocaleUpperCase();
-    if(!sortParams.includes(sorter) || !orderParams.includes(order)){
-        return Promise.reject({
-            status: 400,
-            msg: `Query not allowed`
-        });
-    }
-    if(topic === undefined) {
-        return db
-        .query(`    SELECT  A.author,
+exports.sortArticles = (sorter = `created_at`, order = `DESC`, topic) => {
+  const sortParams = [
+    "author",
+    "title",
+    "article_id",
+    "topic",
+    "created_at",
+    "votes",
+    "comment_count",
+  ];
+  const orderParams = ["ASC", "DESC"];
+  order = order.toLocaleUpperCase();
+  if (!sortParams.includes(sorter) || !orderParams.includes(order)) {
+    return Promise.reject({
+      status: 400,
+      msg: `Query not allowed`,
+    });
+  }
+  if (topic === undefined) {
+    return db
+      .query(
+        `    SELECT  A.author,
                             A.title,
                             A.article_id,
                             A.topic,
@@ -56,25 +63,29 @@ exports.sortArticles = (sorter=`created_at`, order=`DESC`, topic) => {
                         FROM    articles A
                         LEFT JOIN comments B ON A.article_id=A.article_id
                         GROUP BY A.article_id
-                        ORDER BY ${sorter} ${order};`)
-        .then((result) => {
-            return result.rows;
-        })
-    }
-    return db
-    .query(`    SELECT  topic
-                FROM    articles`)
+                        ORDER BY ${sorter} ${order};`
+      )
+      .then((result) => {
+        return result.rows;
+      });
+  }
+  return db
+    .query(
+      `    SELECT  topic
+                FROM    articles`
+    )
     .then((result) => {
-        const topicParams = result.rows.map(thing => thing.topic)
-        const topicWhere = `WHERE A.topic='${topic}'`
-        if(!topicParams.includes(topic)){
-            return Promise.reject({
-                status: 404,
-                msg: `Topic: ${topic}, cannot be found`
-            })
-        }
-    return db
-    .query(`    SELECT  A.author,
+      const topicParams = result.rows.map((thing) => thing.topic);
+      const topicWhere = `WHERE A.topic='${topic}'`;
+      if (!topicParams.includes(topic)) {
+        return Promise.reject({
+          status: 404,
+          msg: `Topic: ${topic}, cannot be found`,
+        });
+      }
+      return db
+        .query(
+          `    SELECT  A.author,
                         A.title,
                         A.article_id,
                         A.topic,
@@ -85,25 +96,29 @@ exports.sortArticles = (sorter=`created_at`, order=`DESC`, topic) => {
                 LEFT JOIN comments B ON A.article_id=A.article_id
                 ${topicWhere}
                 GROUP BY A.article_id
-                ORDER BY ${sorter} ${order};`)
-    .then((result) => {
-        return result.rows;
-    })
-    })
-}
+                ORDER BY ${sorter} ${order};`
+        )
+        .then((result) => {
+          return result.rows;
+        });
+    });
+};
 
 exports.selectUsers = () => {
-    return db
-    .query(`    SELECT  username
-                FROM    users;`)
+  return db
+    .query(
+      `    SELECT  username
+                FROM    users;`
+    )
     .then((result) => {
-        return result.rows;     
-    })
-}
+      return result.rows;
+    });
+};
 
 exports.selectArticleById = (article_id) => {
-    return db
-    .query(`    SELECT  A.author,
+  return db
+    .query(
+      `    SELECT  A.author,
                         A.title,
                         A.article_id,
                         A.body,
@@ -115,50 +130,55 @@ exports.selectArticleById = (article_id) => {
                 LEFT JOIN comments B ON A.article_id=A.article_id
                 WHERE   A.article_id = $1
                 GROUP BY A.article_id;`,
-                [article_id])
-    .then(({rows}) => {
-        const rest = rows[0];
-        if(!rest) {
-          return Promise.reject({
-            status: 400,
-            msg: `No articles with ID: ${article_id}`
-          });
-        }
-        return rest;
-      });
-  };
+      [article_id]
+    )
+    .then(({ rows }) => {
+      const rest = rows[0];
+      if (!rest) {
+        return Promise.reject({
+          status: 400,
+          msg: `No articles with ID: ${article_id}`,
+        });
+      }
+      return rest;
+    });
+};
 
 exports.articleComments = (article_id) => {
-    return db
-    .query(`    SELECT  comment_id,
+  return db
+    .query(
+      `    SELECT  comment_id,
                         votes,
                         created_at,
                         author,
                         body
                 FROM    comments
-                WHERE   article_id=$1`,
-                [article_id])
-    .then(({rows}) => {
-        const rest = rows;
-        if(rest.length === 0) {
-            return Promise.reject({
-            status: 400,
-            msg: `No articles or comments for article with the ID: ${article_id}`
-            })
-        }
-        return rest;
+                WHERE   article_id=$1
+                ORDER BY created_at ASC`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      const rest = rows;
+      if (rest.length === 0) {
+        return Promise.reject({
+          status: 400,
+          msg: `No articles or comments for article with the ID: ${article_id}`,
+        });
+      }
+      return rest;
     });
 };
 
 exports.updateArticleById = (article_id, inc_votes) => {
-    if(!articleIds.includes(parseInt(article_id))) {
-        return Promise.reject({
-        status: 400,
-        msg: `No articles or comments for article with the ID: ${article_id}`
-        })
-    }
-    return db
-    .query(`    UPDATE      articles
+  if (!articleIds.includes(parseInt(article_id))) {
+    return Promise.reject({
+      status: 400,
+      msg: `No articles or comments for article with the ID: ${article_id}`,
+    });
+  }
+  return db
+    .query(
+      `    UPDATE      articles
                 SET         votes = votes + $1
                 WHERE       article_id = $2
                 RETURNING   author,
@@ -167,69 +187,74 @@ exports.updateArticleById = (article_id, inc_votes) => {
                             topic,
                             created_at,
                             votes;`,
-                [inc_votes, article_id])
+      [inc_votes, article_id]
+    )
     .then(({ rows }) => {
-        const rest = rows;
-        if(rest.length === 0) {
-            return Promise.reject({
-            status: 404,
-            msg: `No articles with ID: ${article_id}`
-        })
-    }
-    return rest[0];
+      const rest = rows;
+      if (rest.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No articles with ID: ${article_id}`,
+        });
+      }
+      return rest[0];
     });
-}
+};
 
 exports.deleteComment = (comment_id) => {
-    return db
-        .query(`DELETE FROM     comments 
+  return db
+    .query(
+      `DELETE FROM     comments 
                 WHERE           comment_id = $1
                 RETURNING       *;`,
-                [comment_id])
-        .then(({ rows }) => {
-            const rest = rows;
-            if(rest.length === 0) {
-                return Promise.reject({
-                status: 404,
-                msg: `No comments with ID: ${comment_id}`
-            })
-        }
-        return rows[0]
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      const rest = rows;
+      if (rest.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No comments with ID: ${comment_id}`,
         });
+      }
+      return rows[0];
+    });
 };
 
 exports.insertNewComment = (article_id, username, body) => {
-    if(!usernames.includes(username)) {
-        return Promise.reject({
-            status: 400,
-            msg: `User not registered`
-        })
-    }
-    if(username === undefined || body === undefined) {
-        return Promise.reject({
-            status: 400,
-            msg: `Missing part of post request`
-        })
-    } 
-    if(!articleIds.includes(parseInt(article_id))) {
-        return Promise.reject({
-        status: 400,
-        msg: `No articles or comments for article with the ID: ${article_id}`
-        })
-    }
-    if(typeof username !== `string` || typeof body !== `string`) {
-        return Promise.reject({
-            status: 400,
-            msg: `Usernames and comment bodies should be text`
-            })
-    }
-    return db
-    .query(`    INSERT INTO comments
+  if (!usernames.includes(username)) {
+    return Promise.reject({
+      status: 400,
+      msg: `User not registered`,
+    });
+  }
+  if (username === undefined || body === undefined) {
+    return Promise.reject({
+      status: 400,
+      msg: `Missing part of post request`,
+    });
+  }
+  if (!articleIds.includes(parseInt(article_id))) {
+    return Promise.reject({
+      status: 400,
+      msg: `No articles or comments for article with the ID: ${article_id}`,
+    });
+  }
+  if (typeof username !== `string` || typeof body !== `string`) {
+    return Promise.reject({
+      status: 400,
+      msg: `Usernames and comment bodies should be text`,
+    });
+  }
+  return db
+    .query(
+      `    INSERT INTO comments
                             (body, article_id, author)
                 VALUES      ($1, $2, $3)
                 RETURNING   *;`,
-                [body, article_id, username])
+      [body, article_id, username]
+    )
     .then(({ rows }) => {
-        return rows[0];
-    })
-}
+      return rows[0];
+    });
+};
